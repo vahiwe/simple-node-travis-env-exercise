810a5b09bb1335b79a06e64b2b92b7bdf5a339e8
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2021-05-24 16:10:09 +0000

    Update script and travis configuration

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-
-// function sleep(ms) {
-//   return new Promise(resolve => setTimeout(resolve, ms));
-// }
-
-// async function main() {
-//   while(true) {
-//     console.log("Microservices rock!");
-//     await sleep(5000);
-//   }
-// }
-
-// main();
-
 const favoriteFood = process.env.FAVORITE_FOOD;
-console.log(`My favorite food is ${favoriteFood}`);
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    console.log("Microservices rock!");
+    console.log(`My favorite food is ${favoriteFood}`);
+    await sleep(5000);
+  }
+}
+
+main();
